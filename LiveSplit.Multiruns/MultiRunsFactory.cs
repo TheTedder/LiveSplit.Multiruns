a4bebe6b29a747f98a3e5c5b505ae376a5e883dd
Tedder <25165500+TheTedder@users.noreply.github.com>
2019-05-19 16:12:45 -0400

    add factory

---
+++ LiveSplit.Multiruns/MultiRunsFactory.cs
@@ -0,0 +1,33 @@
+﻿using LiveSplit.Model;
+using LiveSplit.UI.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiveSplit.Multiruns
+{
+    class MultirunsFactory : IComponentFactory
+    {
+        public string ComponentName => "Multiruns";
+
+        public string Description => "allows for marathon-type multiruns with proper load timing and autosplitting";
+
+        public ComponentCategory Category => ComponentCategory.Control;
+
+        public string UpdateName => ComponentName;
+
+        public string XMLURL => "";
+
+        public string UpdateURL => "";
+
+        public Version Version => Assembly.GetExecutingAssembly().GetName().Version;
+
+        public IComponent Create(LiveSplitState state)
+        {
+            return new MultirunsComponent(state);
+        }
+    }
+}
