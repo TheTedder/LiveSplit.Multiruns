e73d5ccc0b78855f175ba87cc725fef522420ee4
Tedder <25165500+TheTedder@users.noreply.github.com>
2019-05-20 16:05:09 -0400

    redesign layout

--- LiveSplit.Multiruns/MultirunsSettings.Designer.cs
+++ LiveSplit.Multiruns/MultirunsSettings.Designer.cs
@@ -33,24 +33,24 @@
             this.tbSplitsFile = new System.Windows.Forms.TextBox();
             this.chkEnable = new System.Windows.Forms.CheckBox();
             this.diaSplitsFile = new System.Windows.Forms.OpenFileDialog();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.gbSplits = new System.Windows.Forms.GroupBox();
             panel1 = new System.Windows.Forms.Panel();
             panel1.SuspendLayout();
-            this.groupBox1.SuspendLayout();
+            this.gbSplits.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            panel1.Controls.Add(this.groupBox1);
+            panel1.Controls.Add(this.gbSplits);
             panel1.Controls.Add(this.chkEnable);
             panel1.Location = new System.Drawing.Point(3, 3);
             panel1.Name = "panel1";
-            panel1.Size = new System.Drawing.Size(444, 144);
+            panel1.Size = new System.Drawing.Size(444, 294);
             panel1.TabIndex = 1;
             // 
             // btnSelect
             // 
-            this.btnSelect.Location = new System.Drawing.Point(212, 17);
+            this.btnSelect.Location = new System.Drawing.Point(6, 19);
             this.btnSelect.Name = "btnSelect";
             this.btnSelect.Size = new System.Drawing.Size(75, 23);
             this.btnSelect.TabIndex = 3;
@@ -59,7 +59,7 @@
             // 
             // tbSplitsFile
             // 
-            this.tbSplitsFile.Location = new System.Drawing.Point(6, 19);
+            this.tbSplitsFile.Location = new System.Drawing.Point(87, 21);
             this.tbSplitsFile.Name = "tbSplitsFile";
             this.tbSplitsFile.Size = new System.Drawing.Size(200, 20);
             this.tbSplitsFile.TabIndex = 2;
@@ -74,16 +74,16 @@
             this.chkEnable.Text = "enable";
             this.chkEnable.UseVisualStyleBackColor = true;
             // 
-            // groupBox1
+            // gbSplits
             // 
-            this.groupBox1.Controls.Add(this.tbSplitsFile);
-            this.groupBox1.Controls.Add(this.btnSelect);
-            this.groupBox1.Location = new System.Drawing.Point(3, 26);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(438, 100);
-            this.groupBox1.TabIndex = 4;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Splits";
+            this.gbSplits.Controls.Add(this.tbSplitsFile);
+            this.gbSplits.Controls.Add(this.btnSelect);
+            this.gbSplits.Location = new System.Drawing.Point(3, 26);
+            this.gbSplits.Name = "gbSplits";
+            this.gbSplits.Size = new System.Drawing.Size(438, 265);
+            this.gbSplits.TabIndex = 4;
+            this.gbSplits.TabStop = false;
+            this.gbSplits.Text = "Splits";
             // 
             // MultirunsSettings
             // 
@@ -91,11 +91,11 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(panel1);
             this.Name = "MultirunsSettings";
-            this.Size = new System.Drawing.Size(450, 150);
+            this.Size = new System.Drawing.Size(450, 300);
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
+            this.gbSplits.ResumeLayout(false);
+            this.gbSplits.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -106,6 +106,6 @@
         private System.Windows.Forms.Button btnSelect;
         private System.Windows.Forms.TextBox tbSplitsFile;
         private System.Windows.Forms.OpenFileDialog diaSplitsFile;
-        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox gbSplits;
     }
 }
